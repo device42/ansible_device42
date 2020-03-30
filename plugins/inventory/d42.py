abac9bd0f3edfd0058ea28d8b376bf08dc6f0d4a
Omar Sanchez <omar.sanchez@device42.com>
2020-03-30 18:05:23 -0400

    fixed some parse issues

--- plugins/inventory/d42.py
+++ plugins/inventory/d42.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
+
 DOCUMENTATION = r'''
-    name: device42.d42.d42
+    module: d42
     plugin_type: inventory
     author:
-      - Will Tome (@willtome)
-      - Anatolii (@achmykhalo)
-      - Omar Sanchez (@Osanchez42)
+        - Will Tome (@willtome)
+        - Omar Sanchez (@osanchez42)
     short_description: Device42 Inventory Plugin
     version_added: "2.10"
     description:
@@ -16,18 +20,18 @@ DOCUMENTATION = r'''
     options:
         plugin:
             description: The name of the Device42 Inventory Plugin, this should always be 'device42.d42.d42'.
-            required: True
+            required: true
             choices: ['device42.d42.d42']
         url:
             description: URI of Device42. The URI should be the fully-qualified domain name, e.g. 'your-instance.device42.net'.
             type: string
-            required: True
+            required: true
             env:
                 - name: D42_URL
         username:
-            description: The Device42 user acount.
+            description: The Device42 user account.
             type: string
-            required: True
+            required: true
             env:
                 - name: D42_USER
         password:
@@ -84,12 +88,12 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         ssl_check = self.get_option('ssl_check')
         strict = self.get_option('strict')
 
-        response = requests.get(f"{base_url}/api/1.0/devices/all", auth=(username, password), verify=ssl_check)
+        response = requests.get(base_url + "/api/1.0/devices/all", auth=(username, password), verify=ssl_check)
         objects = response.json()['Devices']
         for object_ in objects:
             host_name = self.inventory.add_host(to_safe_group_name(object_['name']))
             for k in object_.keys():
-                self.inventory.set_variable(host_name, f'd42_{k}', object_[k])
+                self.inventory.set_variable(host_name, 'd42_' + k, object_[k])
 
             if object_['ip_addresses'] != []:
                 self.inventory.set_variable(host_name, 'ansible_host', object_['ip_addresses'][0]['ip'])
